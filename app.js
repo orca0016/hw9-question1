e8670b6d56c5502e6d9b1a94c00b56de3d3a56bc
Mohammad m <mashhadim901@gmail.com>
2025-04-13 22:47:19 +0430

    feat:validate the rate form input

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const form = document.getElementById('form-add-movie')
 const inputName = document.getElementById('name-input')
 const inputRate = document.getElementById('rate-input')
 const validError = document.getElementById('valid-error')
+const submitForm = document.getElementById('submit-form')
+
+inputRate.addEventListener('input' , (e)=>{
+    const number = Number(e.target.value)
+        if (number > 5 || number < 0 || !number ) {
+        inputRate.classList.add('error-input')
+        validError.innerText = "please enter a valid number between 0 / 5"
+        validError.style.color="red"
+        submitForm.setAttribute('disabled' , true)
+    }else{
+        validError.innerText = ""
+        submitForm.removeAttribute('disabled' )
+        inputRate.classList.remove('error-input')
+    }
+})
