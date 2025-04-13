57085a7a410f101211bdaac534615b0bab572b67
Mohammad m <mashhadim901@gmail.com>
2025-04-13 23:11:23 +0430

    feat:create render row table function

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const inputName = document.getElementById('name-input')
 const inputRate = document.getElementById('rate-input')
 const validError = document.getElementById('valid-error')
 const submitForm = document.getElementById('submit-form')
-
+const data = [{
+    name:'this is a tests' , 
+    rate:4 ,
+    id:  Date.now()
+}]
 inputRate.addEventListener('input' , (e)=>{
     const number = Number(e.target.value)
         if (number > 5 || number < 0 || !number ) {
@@ -20,3 +24,33 @@ inputRate.addEventListener('input' , (e)=>{
         inputRate.classList.remove('error-input')
     }
 })
+
+const renderTableRow=()=>{
+    data.forEach(item=>{
+        const row  = document.createElement('tr')
+        
+        const tdName = document.createElement('td')
+        tdName.innerText=item.name
+        row.appendChild(tdName)        
+        
+        const tdRate = document.createElement('td')
+        tdRate.innerText=item.rate
+        row.appendChild(tdRate)        
+        
+        const button = document.createElement('button')
+        button.innerText = 'Delete'
+        button.classList.add('delete-button')
+        button.addEventListener('click' , ()=> {
+            // do something
+        })
+
+        const tdAction = document.createElement('td')
+        tdAction.appendChild(button)
+        tdAction.classList.add('column-table-delete')
+        row.appendChild(tdAction)
+
+        bodyTable.appendChild(row)
+    })
+}
+
+renderTableRow()
